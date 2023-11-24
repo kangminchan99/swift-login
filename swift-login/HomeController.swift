4038249f2c83d12e47f19cd1ee39f97279ded911
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-11-24 20:38:31 +0900

    swift first

---
+++ swift-login/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  swift-login
+//
+//  Created by minchan on 2023/11/24.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPurple
+        
+    }
+    
+
+}
