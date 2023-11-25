39d47aeaf0b633881df1be098a5eb5ac8a87654f
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-11-25 23:41:04 +0900

    버튼 동작 구현

--- swift-login/LoginController.swift
+++ swift-login/LoginController.swift
@@ -18,14 +18,26 @@ class LoginController: UIViewController {
     
     private let passwordField = CustomTextField(fieldType: .password)
     
+    private let signInButton = CustomButtom(title: "Sign In",hasBackground: true, fontSize: .big)
+    
+    private let newUserButton = CustomButtom(title: "New User? Create Account", fontSize: .med)
+    
+    private let forgotPasswordButton = CustomButtom(title: "Forgot Password?", fontSize: .small)
+    
     // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupUI()
+        
+        self.signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        self.newUserButton.addTarget(self, action: #selector(didTapNewUser), for: .touchUpInside)
+        self.forgotPasswordButton.addTarget(self, action: #selector(didTapForgotPassword), for: .touchUpInside)
 
         // Do any additional setup after loading the view.
     }
+    
+
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -40,10 +52,16 @@ class LoginController: UIViewController {
         self.view.addSubview(headerView)
         self.view.addSubview(usernameField)
         self.view.addSubview(passwordField)
+        self.view.addSubview(signInButton)
+        self.view.addSubview(newUserButton)
+        self.view.addSubview(forgotPasswordButton)
         
         headerView.translatesAutoresizingMaskIntoConstraints = false
         usernameField.translatesAutoresizingMaskIntoConstraints = false
         passwordField.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        newUserButton.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordButton.translatesAutoresizingMaskIntoConstraints = false
 
 
         NSLayoutConstraint.activate([
@@ -61,7 +79,22 @@ class LoginController: UIViewController {
             self.passwordField.topAnchor.constraint(equalTo: usernameField.bottomAnchor, constant: 22),
             self.passwordField.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
             self.passwordField.heightAnchor.constraint(equalToConstant: 55),
-            self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85)
+            self.passwordField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            self.signInButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 22),
+            self.signInButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.signInButton.heightAnchor.constraint(equalToConstant: 55),
+            self.signInButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            self.newUserButton.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 11),
+            self.newUserButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.newUserButton.heightAnchor.constraint(equalToConstant: 44),
+            self.newUserButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
+            
+            self.forgotPasswordButton.topAnchor.constraint(equalTo: newUserButton.bottomAnchor, constant: 6),
+            self.forgotPasswordButton.centerXAnchor.constraint(equalTo: headerView.centerXAnchor),
+            self.forgotPasswordButton.heightAnchor.constraint(equalToConstant: 44),
+            self.forgotPasswordButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85),
 
 
         ])
@@ -71,4 +104,15 @@ class LoginController: UIViewController {
     }
     
     // MARK: - Selectors
+    
+    // 버튼 동작
+    @objc private func didTapSignIn() {
+        print("Debug:", "didtapSignIn")
+    }
+    @objc private func didTapNewUser() {
+        print("Debug:", "didTapNewUser")
+    }
+    @objc private func didTapForgotPassword() {
+        print("Debug:", "didTapForgotPassword")
+    }
 }
